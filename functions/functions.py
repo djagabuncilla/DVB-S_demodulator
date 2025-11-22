5f4d8dc655bf3171ff1cd97d256abc62999a0203
djagabuncilla <sokol.c.c.c.direct@gmail.com>
2025-11-22 09:06:43 +0300

    Готово к представлению

--- functions/functions.py
+++ functions/functions.py
@@ -161,3 +161,40 @@ def fir_filter_apply(signal, taps):
                 y += taps[k] * signal[n - k]
         filtered.append(y)
     return filtered
+
+def polynomial_interp(x_vals, y_vals, x):
+    n = len(x_vals)
+    assert n == len(y_vals)
+    result = 0.0
+    for i in range(n):
+        xi, yi = x_vals[i], y_vals[i]
+        Li = 1.0
+        for j in range(n):
+            if i != j:
+                xj = x_vals[j]
+                Li *= (x - xj) / (xi - xj)
+        result += yi * Li
+    return result
+
+def interpolate_signal(signal, numerator, denominator):
+    interpolated_signal = []
+
+    for counter in range(0, len(signal), denominator):
+        selection = signal[counter:counter + denominator]
+        if len(selection) < denominator:
+            # Можно пропустить или дополнить последний блок
+            continue
+
+        real_part = [z.real for z in selection]
+        imag_part = [z.imag for z in selection]
+
+        x_vals = list(range(len(selection)))
+
+
+        for j in range(numerator):
+            x = j * (len(selection) - 1) / (numerator - 1) if numerator > 1 else 0
+            real_interp = polynomial_interp(x_vals, real_part, x)
+            imag_interp = polynomial_interp(x_vals, imag_part, x)
+            interpolated_signal.append(complex(real_interp, imag_interp))
+
+    return interpolated_signal
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ N = 8192
 signal = read_pcm_file(filename, N) #чтение файла
 carrier_bin = 1441 #частота переноса в бинах 
 signal = frequency_shift(signal, carrier_bin, N) #теперь несущая в центре графика
-# plot_time_signal(signal)
-# building_a_spectrum(signal, N)
+#plot_time_signal(signal)
+#building_a_spectrum(signal, N)
 
 #попыточка фильтрации через FIR-фильтр
 # fc_bins = 380
@@ -17,14 +17,22 @@ signal = frequency_shift(signal, carrier_bin, N) #теперь несущая в
 #теперь мы будем фильтровать сигнал с помощью IFFT
 spectrum = fast_fourier_transform(signal)
 spectrum = fftshift(spectrum)
-spectrum = spectrum.reverse()
+spectrum.reverse() #заморочки с .reverse() - это чисто моя инициатива, чтобы спектр отображался как в спектроанализаторе
 spectrum = freq_cut(spectrum, -380, 380, N)
 spectrum = fftshift(spectrum)
 spectrum.reverse()
 filtered_signal = inverse_fast_fourier_transform(spectrum)
 #проверим сигнал и спектр
+#plot_time_signal(filtered_signal)
+#building_a_spectrum(filtered_signal, N)
+interpolated_signal = interpolate_signal(filtered_signal, 16, 8)  # 10 точек на 3
+print(len(filtered_signal)) #длина фильтрованного сигнала
+print(len(interpolated_signal)) #длина сигнала после интерполяции
 plot_time_signal(filtered_signal)
-plot_spectrum(filtered_signal)
+plot_time_signal(interpolated_signal)
+
+
+
 
 
 
